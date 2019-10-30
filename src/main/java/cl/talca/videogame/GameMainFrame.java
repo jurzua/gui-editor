9d10002cca9f3fb740484723e6b0efd1aba4e050
Ricardo <rurzua990@hotmail.com>
2019-10-30 16:32:03 -0400

    commit

--- src/main/java/cl/talca/videogame/GameMainFrame.java
+++ src/main/java/cl/talca/videogame/GameMainFrame.java
@@ -19,20 +19,10 @@ public class GameMainFrame extends JFrame {
         this.setSize(1000, 600);
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
-        //The AircraftControl neesita como parametro una instancia de la clase Aircraft (el avion)
-        //El avion es creado en la clase GamePanel
-        //GamePanel necesita un metodo que puedes llamar getAircraft que retorne el avion. "public Aircraft getAircraft()..."
-        //ese avion lo tienes que guardar en una variable y entregarselo al contructor de AircraftControl
-        // en la linea "this.addKeyListener(new AircraftControl(null));"
-        // eso es todo
 
         GamePanel myGamePanel = new GamePanel();
         Aircraft avion = myGamePanel.getAircraft();
         this.getContentPane().add(myGamePanel);
-
-        //cuando creamos el constructor en AircraftControl, necesitamos una instancia del objeto Aircraft.
-        //Si vez la siguiente linea, no hay ningun parametro, por eso el error.
-        //Ahora voy a poner ahí un null para que puedas correr el programa
 
         this.addKeyListener(new AircraftControl(avion));
 
