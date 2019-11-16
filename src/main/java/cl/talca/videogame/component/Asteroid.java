5bf2df24762fd7407511a670f7d705ad0373ac2e
Ricardo <rurzua990@hotmail.com>
2019-11-16 13:10:05 -0400

    overlap correction left

--- src/main/java/cl/talca/videogame/component/Asteroid.java
+++ src/main/java/cl/talca/videogame/component/Asteroid.java
@@ -88,7 +88,7 @@ public class Asteroid implements Shape {
     }
 
     static  boolean doOverlap(Point l1, Point r1, Point l2, Point r2) {
-        if (l1.x >= r2.x || l2.x >= r1.x && l1.y <= r2.y || l2.y <= r1.y) {
+        if (l1.x >= r2.x || l2.x <= r1.x && l1.y <= r2.y || l2.y <= r1.y) {
             return false;
         }
         return true;
