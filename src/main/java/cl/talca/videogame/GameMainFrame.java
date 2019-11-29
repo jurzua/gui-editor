b4f44cc5359ca78912d8a7f7cf12109ada5dcb5f
Jorge Urzua <jorge.urzua@akelius.de>
2019-11-29 11:46:27 +0100

    adds intructions

--- src/main/java/cl/talca/videogame/GameMainFrame.java
+++ src/main/java/cl/talca/videogame/GameMainFrame.java
@@ -29,6 +29,23 @@ public class GameMainFrame extends JFrame {
         this.addKeyListener(new GameKeyListener(myGamePanel));
     }
 
+    /**
+     * Hola pesao. No entendiste lo que te escribí. Lo voy a copiar desde Whatsapp:
+     *
+     * "En este código tu creas una instancia de la clase JPanel. Sin embargo, lo que yo te digo (toma como ejemplo la clase GamePanel) es que tienes que crear una nueva clase
+     * llamada "StatisticsPanel" y que extienda (extends) la clase JPanel. Luego el método panelStatistics tiene que crear una instancia de esta nueva clase y retornarla"
+     *
+     * Lo que haces aquí es un método llamado "panelStatistics", pero yo te pido que crees una nueva clase llamada "StatisticsPanel".
+     * Y esa clase tiene que extender "JPanel"
+     *
+     * Por favor sigue estos pasos:
+     * 1. Crea una clase llamada "StatisticsPanel" que extienda "JPanel"
+     * 2. agrega una propiedad privada en StatisticsPanel de tipo "GameStatistics"
+     * 3. crea un constructor en la clase StatisticsPanel que reciba como parametro una instancia de GameStatistics
+     * 4. en el constructor inicializa el panel con color azul y inicializa la propiedad global de tipo GameStatistics
+     * 5. crea un método publico en StatisticsPanel llamada "update" sin contenido por el momento
+     *
+     */
     private JPanel panelStatistics() {
         JPanel panel = new JPanel();
         JButton showLivesCount = new JButton(String.valueOf(this.gameStatistics.showLiveCount()));
